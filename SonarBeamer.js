848e3422df954e6dcaf5c0fc0a1f1321aac05624
adibas03 <adibas03@ymail.com>
2016-07-11 01:02:20 +0100

    Create SonarBeamer.js
    First version with Basic functionalities

---
+++ SonarBeamer.js
@@ -0,0 +1,125 @@
+(function() {
+	"use strict";
+
+	function SonarBeamer(opts) {
+    var errors = [];
+    if(!opts.graphicsArray || opts.graphicsArray.length <1 || (typeof(opts.graphicsArray) != 'object' && typeof(opts.graphicsArray) != 'array'))
+    errors.push('Unset or invalid Graphics Array');
+    else this.graphicsArray = opts.graphicsArray;
+
+		if(!opts.game)
+		errors.push('Phaserjs Game was not referenced');
+		else this.game = opts.game;
+
+    this.grow = 200;
+    if(opts.grow)this.grow = parseFloat(opts.grow);
+
+		this.loop = true;
+		if(typeof(opts.loop) != 'undefined')this.loop = opts.loop;
+
+		this.fade = true;
+		if(typeof(opts.fade) != 'undefined')this.fade = opts.fade;
+
+		if(opts.duration)this.duration = opts.duration;
+
+		this.stweens = [];
+
+		if(errors.length > 0){
+			console.log(errors);
+			return false;
+		}
+
+		this.reset();
+	}
+
+	SonarBeamer.prototype = {
+		reset: function() {
+			if (this.timer) {
+				this.timer.stop();
+			}
+
+			for(i in this.graphicsArray){
+			var anchor_x = this.graphicsArray[i].width;
+			var anchor_y = this.graphicsArray[i].height;
+
+
+			//this.graphicsArray[i].anchor.setTo(anchor_x,anchor_y);
+			this.graphicsArray[i].pivot.set(anchor_x,anchor_y);
+			this.graphicsArray[i].scale.setTo(1,1);
+		  }
+
+			var self = this;
+			this.hasFinished = true;
+
+			this.timer = this.game.time.create(true);
+			this.timer.add(0,beam,this);
+
+				this.timer.onComplete.add(function() {
+					self.hasFinished = true;
+					if (self.onComplete) {
+						self.onComplete();
+					}
+				});
+
+
+		},
+
+		setTime: function(seconds) {
+			this.duration = seconds;
+			this.reset();
+		},
+
+		start: function() {
+			this.reset();
+			this.timer.start();
+		},
+
+		stop: function() {
+			this.timer.stop();
+			for(i in this.stweens){
+				if(this.stweens[i])this.stweens[i].stop();
+			}
+			for(i in this.graphicsArray){
+			this.graphicsArray[i].scale.setTo(1,1);
+			this.graphicsArray[i].alpha = 1;
+			}
+		},
+
+		pause: function() {
+			this.timer.pause();
+			for(i in this.stweens){
+				if(this.stweens[i])this.stweens[i].pause();
+			}
+		},
+
+		resume: function() {
+			this.timer.resume();
+			for(i in this.stweens){
+				if(this.stweens[i])this.stweens[i].resume();
+			}
+		},
+
+	};
+
+
+	function beam() {
+
+	var rep = this.loop?(this.duration?this.duration:-1):0;
+	var del = 0;var speed = 1000;
+
+		for(i in this.graphicsArray){
+			var twn = this.game.add.tween(this.graphicsArray[i].scale).to( { x:this.grow/100, y:this.grow/100 }, speed, Phaser.Easing.Linear.None, true, del, rep, false);
+			this.stweens.push(twn);
+		if(this.fade)var twn = this.game.add.tween(this.graphicsArray[i]).to( { alpha: 0.01 }, speed, Phaser.Easing.Linear.None, true, del, rep, false);
+		this.stweens.push(twn);
+
+		del+=500;
+		}
+
+	}
+
+
+	if (module) {
+		module.exports = SonarBeamer;
+	}
+})();
